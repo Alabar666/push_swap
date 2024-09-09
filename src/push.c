ad30a408191b83a41eaa418933c6e80c2fb1df2e
Hugo Marta <hugollmarta@hotmail.com>
2024-09-09 15:36:49 +0100

    working push

--- src/push.c
+++ src/push.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hluiz-ma <hluiz-ma@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: hugodev <hugodev@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 19:03:16 by hluiz-ma          #+#    #+#             */
-/*   Updated: 2024/09/08 19:04:15 by hluiz-ma         ###   ########.fr       */
+/*   Updated: 2024/09/09 13:09:35 by hugodev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,36 @@
 
 void push(t_stack **dest, t_stack **src)
 {
+    t_stack *node;
     
+    if(!*src)
+        return;
+    node = *src;
+    *src= (*src)->next;
+    if(*src)
+        (*src)->prev = NULL;
+    node->prev = NULL;
+    if(!*dest)
+    {
+        *dest = node;
+        node->next = NULL;
+    }
+    else
+    {
+        node->next = *dest;
+        node->next->prev = node;
+        *dest = node;   
+    }
+}
+
+void pa(t_stack **a, t_stack **b)
+{	
+    push(a, b);
+	write(1, "pa\n", 3);    
+}
+
+void pb(t_stack **b, t_stack **a)
+{	
+    push(b, a);
+	write(1, "pb\n", 3);    
 }
