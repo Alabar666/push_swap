0019d7808c2de3256d25fe9f71819fe5818155fc
Hugo Leonardo Luiz Marta <hluiz-ma@c2r5s2.42porto.com>
2024-09-11 20:47:55 +0100

    upt main func

--- src/main.c
+++ src/main.c
@@ -6,37 +6,50 @@
 /*   By: hluiz-ma <hluiz-ma@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 19:31:27 by hluiz-ma          #+#    #+#             */
-/*   Updated: 2024/09/08 18:36:56 by hluiz-ma         ###   ########.fr       */
+/*   Updated: 2024/09/11 20:37:43 by hluiz-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
+	t_stack	*a;
+	t_stack	*b;
 
-    t_stack *a;
-    t_stack *b; 
+	a = NULL;
+	b = NULL;
+	if (ac < 2 || (ac == 2 && !av[1][0]))
+		return (1);
+	if (ac == 2)
+	{
+		av = ft_split(av[1], ' ');
+		if (av == NULL)
+			return (1);
+		stack_init(&a, av, true);
+	}
+	else
+	{
+		stack_init(&a, av + 1, false);
+	}
+    	print_stacks(a, b);
+    if(!is_sorted(a))
+    {
+        if(stack_len(a) == 2)
+            sa(&a, false);
+        else if (stack_len(a) == 3)
+            sort_three(&a);
+        else
+            push_swap(&a, &b);
+    }
+	print_stacks(a, b);
 
-    a = NULL;
-    b = NULL;
-    if (ac < 2 || (ac == 2 && !av[1][0]))
-        return (1);
     if(ac == 2)
     {
-        av = ft_split(av[1], ' ');
-        if(av == NULL)
-            return 1;
-        stack_init(&a, av);        
-    }  
-    else {
-        stack_init(&a, av+1);
+        free_av(av);
     }
-    print_stacks(a, NULL);
+	free_stack(&a);
+	free_stack(&b);
 
-    free_stack(&a);
-    free_stack(&b);
-    return (0);
+	return (0);
 }
-
-
