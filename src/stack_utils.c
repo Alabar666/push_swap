918578020319a0184ddbc8ee2328b00c3ea5f0ad
Hugo Leonardo Luiz Marta <hluiz-ma@c2r5s2.42porto.com>
2024-09-11 20:46:46 +0100

    upt stack utils

--- src/stack_utils.c
+++ src/stack_utils.c
@@ -1,31 +1,76 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hluiz-ma <hluiz-ma@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/11 14:12:30 by hluiz-ma          #+#    #+#             */
+/*   Updated: 2024/09/11 18:49:13 by hluiz-ma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-t_stack *find_last_node(t_stack *head)
+t_stack	*find_last_node(t_stack *head)
 {
-    if (!head)
-        return (NULL);
-    while (head->next)
-        head = head->next;
-    return (head);
+	if (!head)
+		return (NULL);
+	while (head->next)
+		head = head->next;
+	return (head);
 }
 
-int stack_len(t_stack *stack)
+int	stack_len(t_stack *stack)
 {
-    int len;
-    t_stack *temp;
+	int		len;
+	t_stack	*temp;
 
-    temp = stack;
+	temp = stack;
 	len = 0;
-    while (temp && temp->prev)
-    {
-        temp = temp->prev;
-    }
-    while (temp)
-    {
-        len++;
-        temp = temp->next;
-    }
-    return len;
+	while (temp && temp->prev)
+	{
+		temp = temp->prev;
+	}
+	while (temp)
+	{
+		len++;
+		temp = temp->next;
+	}
+	return (len);
 }
+
+t_stack	*find_small(t_stack *stack)
+{
+	long	small;
+	t_stack	*small_node;
+
+	if (!stack)
+		return (NULL);
+	small = LONG_MAX;
+	while (stack)
+	{
+		if (stack->nbr < small)
+		{
+			small = stack->nbr;
+			small_node = stack;
+		}
+		stack = stack->next;
+	}
+	return (small_node);
+}
+
+t_stack	*return_cheap(t_stack *stack)
+{
+	if (!stack)
+		return (NULL);
+	while (stack)
+	{
+		if (stack->cheap)
+			return (stack);
+		stack = stack->next;
+	}
+	return (0);
+}
+
+
